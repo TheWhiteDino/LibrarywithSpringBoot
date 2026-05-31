ae9e36df9639e17ddb9e72fd6a0b851aa3645883
SimonDubasqueETS <simon.dubasque.1@ens.etsmtl.ca>
2026-05-31 15:20:58 +0200

    entity + Controller + Repositroty - need to order

--- src/main/java/com/example/demo/BookRestController.java
+++ src/main/java/com/example/demo/BookRestController.java
@@ -1,14 +1,37 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 @RestController 
 public class BookRestController {
 
-    @GetMapping("/test")
-    public String hellowWorld{
+    private final BookRepository bookRepository;
+
+    public BookRestController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    @GetMapping("/book")
+    public String get() {
+        BookEntity newBook = BookEntity.builder()
+                                       .title("DungeonCrawlerCarl")
+                                       .pages(510)
+                                       .build();
+
+        log.info("New book created: {}", newBook);
+
+        bookRepository.save(newBook);
+        
         return "Hello World";
     }
+
+
 
 
 
